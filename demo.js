e4e90e282c94ea7217d7699783950ca8e68196f4
Hassan Mahfuj <humahfuj@gmail.com>
2024-01-16 18:13:39 +0600

    first commit

---
+++ demo.js
@@ -0,0 +1,79 @@
+// var num1 = prompt("enter your first number");
+// var num2 = prompt("enter your second number");
+// var sum , sub ;
+// num1 = parseInt(num1 , 10);
+// num2 = parseInt(num2 , 10);
+
+// sum = num1 + num2 ;
+// document.write("additon =" + sum + "<br/>");
+
+// sub = num1 - num2;
+// document.write("subtraction =" + sub);
+
+
+// var num1 = prompt("enter your first number");
+// var num2 = prompt("enter your second number");
+// var sum,sub;
+// num1 = parseInt(num1,10);
+// num2 = parseInt(num2,10);
+// sum = num1 + num2;
+// document.write("addition = " + sum + "<br/>");
+
+// sub = num1 - num2;
+// document.write("subtraction =" + sub);
+
+
+// var x = 25;
+// var y = 30;
+// x += 30
+// document.write(x);
+// console.log(x);
+
+// var num1 = prompt("enter your first number");
+// var num2 = prompt("enter your second number");
+// var sum,sub;
+// num1 = parseInt(num1,10);
+// num2 = parseInt(num2,10);
+
+// sum = num1 + num2;
+// document.write("addition =" + sum + "<br/>");
+
+// sub = num1 - num2;
+// document.write("subtraction =" + sub + "<br/>");
+
+// sum = num1 * num2;
+// document.write("multipliction =" + sum + "<br/>");
+
+// sum = num1 / num2;
+// document.write("division =" + sum + "<br/>");
+
+// sub = num1 % num2;
+// document.write("expnantiation =" + sub + "<br/>");
+
+// sum = num1 + num2;
+// document.write(num1 + "+" + num2 + "=" + sum + "<br/>");
+
+// sub = num1 - num2;
+// document.write(num1 + "-" + num2 + "=" + sub );
+
+
+// var base = parseFloat(prompt("enter base ="));
+// var height = parseFloat(prompt("enter height ="));
+
+// var area = base * height;
+// document.write("area =" + area + "<br/>");
+
+
+// var base = parseFloat(prompt("enter base ="));
+// var height = parseFloat(prompt("enter height ="));
+
+// var area = (base * height) /2;
+// document.write("area =" + area + "<br/>");
+
+
+var x = ["farzana","arfan","rafia"];
+console.log(x);
+x.push("tangila","sharmin");
+console.log(x);
+x.pop();
+console.log(x)
