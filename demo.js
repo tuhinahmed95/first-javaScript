c11057a346c11271ceedf54bfbaad0229250bf54
Hassan Mahfuj <humahfuj@gmail.com>
2024-01-22 15:06:28 +0600

    js

--- demo.js
+++ demo.js
@@ -123,35 +123,133 @@
 // console.log("failed");
 
 
-function addition(a,b) { 
-    document.write("addition =" + (a + b));
-}
-addition(10,20);
-document.write("<br/>");
+// function addition(a,b) { 
+//     document.write("addition =" + (a + b));
+// }
+// addition(10,20);
+// document.write("<br/>");
 
-function subtration(x,y) { 
-    document.write("subtraction =" + (x-y));
-}
-subtration(20,10);
-document.write("<br/>");
+// function subtration(x,y) { 
+//     document.write("subtraction =" + (x-y));
+// }
+// subtration(20,10);
+// document.write("<br/>");
 
-function divide(e,f) { 
-    document.write("divide =" + e/f);
-}
-divide(40,5);
-document.write("<br/>");
+// function divide(e,f) { 
+//     document.write("divide =" + e/f);
+// }
+// divide(40,5);
+// document.write("<br/>");
 
-function multipliction(rafia,farzana) { 
-    document.write("multiplication :" + rafia*farzana);
-}
-multipliction(7,8)
-document.write("<br/>");
+// function multipliction(rafia,farzana) { 
+//     document.write("multiplication :" + rafia*farzana);
+// }
+// multipliction(7,8)
+// document.write("<br/>");
 
-function modulas(c,d) { 
-    document.write("modulas =" + c%d);
+// function modulas(c,d) { 
+//     document.write("modulas =" + c%d);
+// }
+// modulas(100,25)
+// document.write("<br/>")
+
+// var x = subtraction(10,20);
+// document.write(x + "<br/>")
+
+// function subtraction(a,b) { 
+//     return a-b;
+// }
+
+// var y = addition(15,45);
+// document.write(y);
+
+// function addition(x,y) { 
+//     return x+y;
+// }
+
+
+var number = prompt("enter your number :");
+if (number ==0)
+document.write("zero");
+else if (number == 1)
+document.write("one");
+else if (nember == 2)
+document.write("two");
+else if (number == 3)
+document.write("three");
+else if (number == 4)
+document.write("four");
+else if (number == 5)
+document.write("five");
+else if (number == 6)
+document.write("six");
+else if (number == 7)
+document.write("seven");
+else if (number == 8)
+document.write("eight");
+else if (number == 9)
+document.write("nine");
+else
+document.write("not a number");
+
+
+var letter = prompt("enter a letter :");
+switch (letter) { 
+    case "0":
+        document.write("zero");
+        break;
+    case "1":
+        document.write("one");
+        break;
+    case "2":
+        document.write("two");
+        break;
+    case "3":
+        document.write("three");
+        break;
+    case "4":
+        document.write("four");
+        break;
+    case "5":
+        document.write("five");
+        break;
+    case "6":
+        document.write("six");
+        break;
+    case "7":
+        document.write("seven");
+        break;
+    case "8":
+        document.write("eight");
+        break;
+    case "9":
+        document.write("nine");
+        break;
+    case "10":
+        document.write("ten");
+        break;
+    case "11":
+        document.write("eleven");
+        break;
+    case "12":
+        document.write("tweleve");
+        break;
+    case "13":
+        document.write("thirteen");
+        break;
+    case "14":
+        document.write("fourteen");
+        break;
+    case "15":
+        document.write("fiftin");
+     
+    default:
+        document.write("not a number");    
 }
-modulas(100,25)
-document.write("<br/>")
+
+
+
+
 
 
 
