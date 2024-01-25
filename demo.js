108a475ff73cbf3e81c2e10541ac67fa4e6a7918
Hassan Mahfuj <humahfuj@gmail.com>
2024-01-25 16:35:29 +0600

    arr and loop

--- demo.js
+++ demo.js
@@ -168,89 +168,280 @@
 // }
 
 
-var number = prompt("enter your number :");
-if (number ==0)
-document.write("zero");
-else if (number == 1)
-document.write("one");
-else if (nember == 2)
-document.write("two");
-else if (number == 3)
-document.write("three");
-else if (number == 4)
-document.write("four");
-else if (number == 5)
-document.write("five");
-else if (number == 6)
-document.write("six");
-else if (number == 7)
-document.write("seven");
-else if (number == 8)
-document.write("eight");
-else if (number == 9)
-document.write("nine");
-else
-document.write("not a number");
+// var number = prompt("enter your number :");
+// if (number ==0)
+// document.write("zero");
+// else if (number == 1)
+// document.write("one");
+// else if (nember == 2)
+// document.write("two");
+// else if (number == 3)
+// document.write("three");
+// else if (number == 4)
+// document.write("four");
+// else if (number == 5)
+// document.write("five");
+// else if (number == 6)
+// document.write("six");
+// else if (number == 7)
+// document.write("seven");
+// else if (number == 8)
+// document.write("eight");
+// else if (number == 9)
+// document.write("nine");
+// else
+// document.write("not a number");
 
 
-var letter = prompt("enter a letter :");
-switch (letter) { 
-    case "0":
-        document.write("zero");
-        break;
-    case "1":
-        document.write("one");
-        break;
-    case "2":
-        document.write("two");
-        break;
-    case "3":
-        document.write("three");
-        break;
-    case "4":
-        document.write("four");
-        break;
-    case "5":
-        document.write("five");
-        break;
-    case "6":
-        document.write("six");
-        break;
-    case "7":
-        document.write("seven");
-        break;
-    case "8":
-        document.write("eight");
-        break;
-    case "9":
-        document.write("nine");
-        break;
-    case "10":
-        document.write("ten");
-        break;
-    case "11":
-        document.write("eleven");
-        break;
-    case "12":
-        document.write("tweleve");
-        break;
-    case "13":
-        document.write("thirteen");
-        break;
-    case "14":
-        document.write("fourteen");
-        break;
-    case "15":
-        document.write("fiftin");
+// var letter = prompt("enter a letter :");
+// switch (letter) { 
+//     case "0":
+//         document.write("zero");
+//         break;
+//     case "1":
+//         document.write("one");
+//         break;
+//     case "2":
+//         document.write("two");
+//         break;
+//     case "3":
+//         document.write("three");
+//         break;
+//     case "4":
+//         document.write("four");
+//         break;
+//     case "5":
+//         document.write("five");
+//         break;
+//     case "6":
+//         document.write("six");
+//         break;
+//     case "7":
+//         document.write("seven");
+//         break;
+//     case "8":
+//         document.write("eight");
+//         break;
+//     case "9":
+//         document.write("nine");
+//         break;
+//     case "10":
+//         document.write("ten");
+//         break;
+//     case "11":
+//         document.write("eleven");
+//         break;
+//     case "12":
+//         document.write("tweleve");
+//         break;
+//     case "13":
+//         document.write("thirteen");
+//         break;
+//     case "14":
+//         document.write("fourteen");
+//         break;
+//     case "15":
+//         document.write("fiftin");
      
-    default:
-        document.write("not a number");    
+//     default:
+//         document.write("not a number");    
+// // }
+
+
+//       for ( var x =1; x <=10; x = x+1){ 
+//     document.write("<h1>Bangladesh<h1/>");
+// }
+
+//     document.write("<h1>End<h1/>");
+    
+    
+
+
+
+// x.sort("south korea");
+// console.log(x);
+
+
+
+
+// var letter = prompt("enter your first letter :");
+// letter =letter.toLowerCase();
+// switch(letter){ 
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+    
+//         document.write("vowel");
+//         break;
+//         default:
+//             document.write("consonant");
+//  }
+
+
+
+
+
+
+// var marks = prompt("enter your marks");
+// if (marks >=80 && marks <=100){console.log("A+"); }
+
+// else if (marks >=70)
+// console.log("A");
+// else if (marks >=60)
+// console.log("A-");
+// else if (marks >=50)
+// console.log("B")
+// else if (marks >=40)
+// console.log("D");
+// else if (marks >=33)
+// console.log("D");
+// else 
+// console.log("failed");
+
+
+
+// var score = parseFloat(prompt("enter your number :"));
+// if (score >=90 && score <=100){
+//     document.write("A+") 
+
+// }
+
+// var score = parseInt(prompt("enter your number :"));
+// if (score >=80 && score <= 100){ 
+//     document.write("A+");
+// }
+// else if (score >=70 && score <=79){ 
+//     document.write("A");
+// }
+// else if (score >=60 && score <=69){ 
+//     document.write("A-");
+// }
+// else if(score >=50 && score <=59){ 
+//     document.write("B")
+// }
+// else if(score >=40 && score <=49){ 
+//     document.write("C");
+// }
+// else if(score <=33 && score >=39){ 
+//     document.write("D");
+// }
+// else
+// document.write("Failed");
+
+
+
+// var score = parseInt(prompt("enter your number :"));
+// if (score >=80 && score >=100){ 
+//     document.write("A+");
+// }
+// else if (score >=70 && score >=79){ 
+//     document.write("A");
+// }
+// else if (score >=60 && score <=69){ 
+//     document.write("A-");
+// }
+// else if (score >=50 && score <=59){ 
+//     document.write("B");
+// }
+// else if(score >=40 && score <=49){ 
+//     document.write("C");
+// }
+// else if (score >=33 && score <=39){ 
+//     document.write("D");
+// }
+// else
+// document.write("Failed");
+
+
+// var number = prompt("enter your numbr :");
+// if (number >=80 && number <=100){ 
+//     document.write("A+");
+// }
+// var number = prompt("enter your numbr :");
+// if (number ==1)
+// document.write("zero");
+// else if (number == 1)
+// document.write("one");
+// else if (number == 2)
+// document.write("two");
+// else if (number == 3)
+// document.write("three");
+// else if (number == 4)
+// document.write("four");
+// else if (number ==5)
+// document.write("five");
+// else if (number == 6)
+// document.write("six");
+// else if (number == 7)
+// document.write("seven");
+// else if (number == 8)
+// document.write("eight");
+// else if (number == 9)
+// document.write("nine");
+// else if (number == 10)
+// document.write("teen");
+// else
+// document.write("not a number");
+
+// var x = 1;
+// var sum = 0;
+// while(x <=5){ 
+//     if (x % 3 == 0 && x % 5 == 0){ 
+//         document.write(" " + x);
+//         sum = sum + x;
+//     }
+//     x = x + 1;
+// }
+
+
+// var marks = parseInt(prompt("enter your number : "));
+// if (marks >= 80 && marks >=100){ 
+//     document.write("A+");
+// }
+
+// else if (marks >=70 && marks <=79){ 
+//     document.write("A");
+// }
+
+
+// var marks = prompt("enter your number :");
+// switch (true){ 
+//     case (marks >=90 && marks <=100):
+//         document.write("A+");
+//         break;
+//     case (marks >=80 && marks <=89):
+//         document.write("A");
+//         break;
+//     case (marks >=70 && marks <=79):
+//         document.write("b");
+//         break;
+//     case (marks >=60 && marks <= 69):
+//         document.write("c");
+//         break;
+//     case (marks >=101):
+//         document.write("you are a genious");
+//         break;
+//         default:
+//             document.write("fail");          
+
+// }
+
+var x =1;
+var sum = 0;
+while(x<=50){ 
+    if (x % 3 ==0 && x % 5 ==0){ 
+        document.write(" " + x);
+        sum = sum + x;
+    }
+    
+    x = x + 1;
 }
-
-
-
-
-
-
-
-
+document.write("sum =" + sum);
+const arr = ["a" , "b" , "c" , "d" , "e" , "f"];
+console.log(arr)
+for (let x =0; x <arr.length; x = x +1){
+    document.write(arr);
+    
+}
