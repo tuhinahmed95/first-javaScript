bf73e83fed9f8712c135723ec4b196b4d37a3ec7
Hassan Mahfuj <humahfuj@gmail.com>
2024-02-12 19:02:14 +0600

    update

---
+++ cs-hl-cl/index.js
@@ -0,0 +1,12 @@
+
+let myVar = document.querySelector("#paraid");
+function display1(){
+
+    myVar.classList.add("style-c");
+
+ }
+
+ function display2(){ 
+    
+    myVar.classList.remove("style-c");
+ }
