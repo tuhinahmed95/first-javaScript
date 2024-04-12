6dd3294bd6cfbd7ba981cbe2c9d8e26b08bd04a8
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-04-12 11:44:31 +0600

    date

--- DOM JS/Change_event_Js/change.js
+++ DOM JS/Change_event_Js/change.js
@@ -9,13 +9,20 @@
 //     console.log(event.target.value);
 // }
 
-const programs = document.querySelectorAll("input[name=program]");
-Array.from(programs).map((programs)=>{ 
-    programs.addEventListener("change", programHandler);
-});
 
-function programHandler(e){ 
-    if(e.target.checked){ 
-        console.log(e.target.value);
-    }
-}
+
+// const programs = document.querySelectorAll("input[name=program]");
+// Array.from(programs).map((programs)=>{ 
+//     programs.addEventListener("change", programHandler);
+// });
+
+// function programHandler(e){ 
+//     if(e.target.checked){ 
+//         console.log(e.target.value);
+//     }
+// }
+
+
+
+const dropdown = document.querySelector("department");
+console.log(department);
