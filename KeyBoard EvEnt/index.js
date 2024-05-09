d63b198717d76a9151ad22067fcf344e743868cf
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-09 22:45:55 +0600

    focus event

---
+++ KeyBoard EvEnt/index.js
@@ -0,0 +1,23 @@
+
+
+const textarea = document.querySelector("textarea");
+
+// textarea.addEventListener('keydown', function(){ 
+//     console.log('keydown');
+// })
+
+// textarea.addEventListener('keypress', function(){ 
+//     console.log('keypress');
+// })
+
+// textarea.addEventListener('keyup', function(e){ 
+//     console.log('e.key');
+// })
+
+// textarea.addEventListener('keyup', function(e){ 
+//     console.log('e.code');
+// })
+
+textarea.addEventListener('keyup', function(e){ 
+    console.log('e.shiftkey');
+})
---
+++ focus-event/focus-event.js
@@ -0,0 +1,24 @@
+
+
+const input = document.querySelector("input");
+
+input.addEventListener('blur', function(e){ 
+    // console.log('e.target.value');
+    // input.value = e.target.value.toUpperCase();
+})
+
+
+// input.addEventListener('focus', function(){ 
+//     input.style.backgroundColor="red";
+//     input.style.padding = "2rem";
+// })
+
+
+// input.addEventListener('focusin', function(){ 
+//     console.log('focusin is occured');
+// })
+
+
+// input.addEventListener('focusout', function(){ 
+//     console.log('focusout is occured');
+// })
