89e539083dc96189b917afdd75ef36b081bb626e
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-11 00:45:03 +0600

    for forEach

--- event method/settimeout_setinner.js
+++ event method/settimeout_setinner.js
@@ -25,16 +25,48 @@
 
 // setInterval method
 
+// let saveButton = document.querySelector(".save-btn");
+// let message = document.querySelector(".message");
+// saveButton.addEventListener("click", startCount);
+
+// function startCount(){ 
+//     let count = 1;
+//     message.textContent = count;
+
+//     setInterval(() => {
+//         count ++;
+//         message.textContent = count;
+//     }, 1000);
+// }
+
+
+
+
 let saveButton = document.querySelector(".save-btn");
 let message = document.querySelector(".message");
-saveButton.addEventListener("click", startCount);
 
-function startCount(){ 
-    let count = 1;
-    message.textContent = count;
+saveButton.addEventListener("click", startClock);
 
-    setInterval(() => {
-        count ++;
-        message.textContent = count;
-    }, 1000);
+function startClock(){ 
+
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+
+        minutes = formateTime(minutes);
+        seconds = formateTime(seconds);
+    let time = hours + ":" + minutes + ":" + seconds;
+    
+
+    message.textContent = time;
+    setInterval(startClock, 1000);
+}
+
+function formateTime(value){ 
+    if(value <10){ 
+        value = "0" + value;
+
+    }
+
 }
---
+++ handle-error/handle_error.js
@@ -0,0 +1,26 @@
+
+
+// handle error syntax
+
+try{ 
+
+    // code
+    alert("hi everyone");
+
+    //  alert(x); this is a error
+
+}catch(error){ 
+    // handle error
+    alert("inside catch block");
+    console.log("inside catch block");
+    console.log(error);
+    // console.log(error.name); error name print korbr
+    // console.log(error.message); error message print korbr
+
+
+} finally{ 
+
+    // alert("Bye Everyone"); sob somoy kaj korbe
+    
+}
+
