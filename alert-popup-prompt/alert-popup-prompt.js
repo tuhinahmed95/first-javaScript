aed384dc59b2de3073ce1fa9ce3482daccd74aaa
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-10 14:58:59 +0600

    popup

---
+++ alert-popup-prompt/alert-popup-prompt.js
@@ -0,0 +1,18 @@
+
+// console.log(window);
+// alert("error here");
+// confirm("are your sure");
+
+// use popup 
+
+// function deleteSomething(){ 
+//     let value = confirm("do you want to delte something?");
+//     if(value){ 
+//         console.log("delete succes");
+//     }
+//     else{ 
+//         console.log("delete is not success");
+//     }
+// }
+
+// deleteSomething();
