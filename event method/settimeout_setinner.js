4b41d793b1608ce0b885e2e3bd474f6b9ba3b1fe
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-05-10 15:49:47 +0600

    event method

--- event method/settimeout_setinner.js
+++ event method/settimeout_setinner.js
@@ -9,10 +9,32 @@
 //     console.log("hello");
 // }
 
+// let saveButton = document.querySelector(".save-btn");
+// let message = document.querySelector(".message");
+// saveButton.addEventListener("click", saveUser);
+
+// function saveUser(){ 
+//     message.textContent = "User Registration Succesfull";
+
+//     setTimeout(() => {
+//         message.textContent = "";
+//     }, 2000);
+// }
+
+
+
+// setInterval method
+
 let saveButton = document.querySelector(".save-btn");
 let message = document.querySelector(".message");
-message.addEventListener("click", saveUser);
+saveButton.addEventListener("click", startCount);
 
-function saveUser(){ 
-    message.textContent = "User Registration Succesfull";
+function startCount(){ 
+    let count = 1;
+    message.textContent = count;
+
+    setInterval(() => {
+        count ++;
+        message.textContent = count;
+    }, 1000);
 }
