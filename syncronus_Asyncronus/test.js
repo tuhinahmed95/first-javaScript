68d9fab63322ddbf0499473c35bcf3057e263668
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-06-04 22:57:51 +0600

    asynconus task

--- syncronus_Asyncronus/test.js
+++ syncronus_Asyncronus/test.js
@@ -5,3 +5,13 @@ console.log("task 2");
 const tasTwo = () => { 
     console.log("task3");
 }
+const taskThree = () => { 
+    console.log("task4");
+}
+const taskFour = () => { 
+    console.log("task5");
+}
+taskOne();
+tasTwo();
+taskThree();
+taskFour();
