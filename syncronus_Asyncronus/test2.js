9b9ed51c2afa034c24c14508263b98986354b5a6
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-06-05 10:29:12 +0600

    callback

--- syncronus_Asyncronus/test2.js
+++ syncronus_Asyncronus/test2.js
@@ -4,3 +4,12 @@ function square(x){
     console.log('square of ${x}: ${x * x}');
 }
 square(5);
+
+// const y = square;
+// y(5);
+
+function higherOrder(num,callback){ 
+    callback(num);
+
+}
+higherOrder(6,square);
