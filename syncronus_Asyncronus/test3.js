dd305e71c9cce567c85f430e92e381f330da9169
Md Tuhin Mia <mtuhinahmed95@gmail.com>
2024-06-05 23:05:08 +0600

    promise

--- syncronus_Asyncronus/test3.js
+++ syncronus_Asyncronus/test3.js
@@ -2,14 +2,20 @@ console.log("welcome");
 
 // how to create a promise =>pending, resolve,reject .
 const promise1 = new Promise((resolve,pending)=>{ 
-    let promisCompleted = true;
+    let promisCompleted = false;
     if(promisCompleted){ 
-        resolve("promise completed");
+        resolve("promise 1 completed");
     }else{ 
-        reject("not completed");
+        reject("not completed promise 1");
     }
 
 })
-console.log(promise1);
+//console.log(promise1);
+promise1.then((res)=>{ 
+    console.log(res);
+})
+catch((err) =>{ 
+    console.log(err);
+});
 
 console.log("end");
