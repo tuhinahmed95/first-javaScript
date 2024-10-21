26dfda404b7d46a3d12a2fdc7e2573a08590cd70
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-10-21 13:07:16 +0600

    js

--- area_various_shapes/index.js
+++ area_various_shapes/index.js
@@ -1,5 +1,12 @@
-let farn = parseFloat(prompt("enter farn ="));
 
-let cel = (farn-32) * (5/9);
+// find celciou 
 
-document.write("Celcius = "+ cel);
+// let farn = parseFloat(prompt("enter farn ="));
+
+// let cel = (farn-32) * (5/9);
+
+// document.write("Celcius = "+ cel);
+
+let cel = parseFloat(prompt("enter celcious ="));
+let farn = (cel*(9/5) ) +32;
+document.write("Farenheigh ="+ farn);
