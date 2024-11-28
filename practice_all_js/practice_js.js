cf3d9148a594bbfca5342a2a06c473dd36e68e62
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-28 20:54:07 +0600

    js

---
+++ practice_all_js/practice_js.js
@@ -0,0 +1,7 @@
+let marks = parseFloat(prompt("enter your marks"));
+
+if(marks >=80 && marks<=100 ){ 
+    document.write("Your Result Is =  A+");
+}else if(marks >=70 || marks <=80){ 
+    document.write("A");
+}
