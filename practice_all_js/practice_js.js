0b745c9473ea0c59bd701ece798990f47c02efe2
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-11-29 10:08:26 +0600

    php

--- practice_all_js/practice_js.js
+++ practice_all_js/practice_js.js
@@ -1,7 +1,31 @@
-let marks = parseFloat(prompt("enter your marks"));
+// let marks = parseFloat(prompt("enter your marks"));
 
-if(marks >=80 && marks<=100 ){ 
-    document.write("Your Result Is =  A+");
-}else if(marks >=70 || marks <=80){ 
-    document.write("A");
-}
+// if(marks >=80 && marks<=100 ){ 
+//     document.write("Your Result Is =  A+");
+// }else if(marks >=70 || marks <=80){ 
+//     document.write("A");
+// }
+
+document.write("<br/>");
+
+var a,b,c;
+a = 9;
+b = 6;
+c = a+b;
+document.write(c);
+document.write("<br/>");
+
+// alert("Hi,Tuhin");
+document.write("<br/>");
+
+let name,address,profession,status,allv;
+name = "Tuhin";
+address = "Gaibandha";
+profession = "Developer";
+status = "Single";
+allv = name + address + profession + status;
+document.write(allv,' ');
+
+
+
+
