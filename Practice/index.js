ff7cf06aadf9296abe60976953c1fb355b240a2b
tuhinahmed95 <mtuhinahmed95@gmail.com>
2024-12-10 20:37:55 +0600

    js

--- Practice/index.js
+++ Practice/index.js
@@ -1,4 +1,22 @@
-function showAlert (){ 
-    let userName = document.getElementById("text_user_name").value;
-    alert("User Name : " + userName);
-}
+// function showAlert (){ 
+//     let userName = document.getElementById("text_user_name").value;
+//     alert("User Name : " + userName);
+// }
+
+// <br/>
+
+// let num = 16 + "tuhin";
+// document.write(typeof(num));<br/>
+// let num1 = "18"+"ahmed";
+// document.write(num1);
+
+// var num = 39 + 4 + 'tuhin';
+// document.write(num);
+
+// function Myfunction(x,y){ 
+//     return x * y;
+// }
+
+// let result = Myfunction(4,5);
+// document.getElementById("demo").innerHTML=result;
+// alert('tuhin');
